26d7d583e871bc863675a92c86e06a9c8e5f7204
Adrian Rojas <adrian.rojasarias@ucr.ac.cr>
2021-07-03 21:03:46 -0600

    Errased comments on main

--- Proyecto2/src/concurrent_forest/main.cpp
+++ Proyecto2/src/concurrent_forest/main.cpp
@@ -6,33 +6,3 @@
 int main(int argc, char* argv[]) {
   return Mago().start(argc, argv);
 }
-
-/*
-main(){
-    controlador.start();
-
-}
-
-Shared_data{
-    Bosque original
-    NuevoBosque
-}
-
-controlador(char[][] mapa){
-    for(noches){
-        actualizarbosque(SharedData);
-    }
-    filecreate(NuevoBosque);
-    shared_data->BosqueOriginal = NuevoBosque;
-}
-
-bosque(){
-    actualizarbosque(){
-        createthreads(actualizarCelda, Bosqueoriginal)
-    }
-
-    actualizarCelda(void * data){
-        AplicarReglas();
-        nuevoBosque[0][0] = 'a';
-    }
-}*/
